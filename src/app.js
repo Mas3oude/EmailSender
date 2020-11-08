4f929311483ee3a091faf2e07abaf3eb51ad3deb
Mohamed <eng.massoud@hotmail.com>
2020-11-08 22:33:52 +0100

    working fine with secured external emails domains

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 // async..await is not allowed in global scope, must use a wrapper
 async function main() {
-// try{
+ try{
 
 
 
@@ -15,18 +15,23 @@ async function main() {
       const emailSecure = process.env.Emailsecure || true;
       const emailUser = process.env.EmailUser || "";
       const emailPass = process.env.EmailPass || "";
+      const rejectUnauthorized = process.env.rejectUnauthorized || false;
 
       console.log(`${new Date()}, email configurations : 
                    emailHost : ${emailHost} \n
                    emailPort : ${emailPort} \n
                    emailSecure: ${emailSecure} \n
                    emailUser : ${emailUser} \n
-                   emailPass : ${emailPass}`);
+                   emailPass : ${emailPass} \n 
+                   rejectUnauthorized : ${rejectUnauthorized}` );
       // create reusable transporter object using the default SMTP transport
       let transporter = nodemailer.createTransport({
         host: emailHost,
         port: emailPort,
-        secure: true, // true for 465, false for other ports
+        secure: emailSecure, // true for 465, false for other ports
+        tls:{
+          rejectUnauthorized: false
+      },
         auth: {
           user: emailUser,
           pass: emailPass
@@ -36,10 +41,114 @@ async function main() {
       // send mail with defined transport object
       let info = await transporter.sendMail({
         from: emailUser,
-        to: "eng.massoud@hotmail.com", // list of receivers
-        subject: "Hello ✔", // Subject line
+        to: "eng.massoud@hotmail.com,ondrej.ilek@ternologic.com,ahmad.herzallah@ternologic.com", // list of receivers
+        subject: "Hello", // Subject line
         text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
+        html:`<!-- #######  YAY, I AM THE SOURCE EDITOR! #########-->
+        <h1 style="color: #5e9ca0;">Are you Ready for your virtual life?</h1>
+        <h2 style="color: #2e6c80;">How to use the editor:</h2>
+        <p>Paste your documents in the visual editor on the left or your HTML code in the source editor in the right. <br />Edit any of the two areas and see the other changing in real time.&nbsp;</p>
+        <p>Click the <span style="background-color: #2b2301; color: #fff; display: inline-block; padding: 3px 10px; font-weight: bold; border-radius: 5px;">Clean</span> button to clean your source code.</p>
+        <h2 style="color: #2e6c80;">Some useful features:</h2>
+        <ol style="list-style: none; font-size: 14px; line-height: 32px; font-weight: bold;">
+        <li style="clear: both;"><img style="float: left;" src="https://html-online.com/img/01-interactive-connection.png" alt="interactive connection" width="45" /> Interactive source editor</li>
+        <li style="clear: both;"><img style="float: left;" src="https://html-online.com/img/02-html-clean.png" alt="html cleaner" width="45" /> HTML Cleaning</li>
+        <li style="clear: both;"><img style="float: left;" src="https://html-online.com/img/03-docs-to-html.png" alt="Word to html" width="45" /> Word to HTML conversion</li>
+        <li style="clear: both;"><img style="float: left;" src="https://html-online.com/img/04-replace.png" alt="replace text" width="45" /> Find and Replace</li>
+        <li style="clear: both;"><img style="float: left;" src="https://html-online.com/img/05-gibberish.png" alt="gibberish" width="45" /> Lorem-Ipsum generator</li>
+        <li style="clear: both;"><img style="float: left;" src="https://html-online.com/img/6-table-div-html.png" alt="html table div" width="45" /> Table to DIV conversion</li>
+        </ol>
+        <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
+        <h2 style="color: #2e6c80;">Cleaning options:</h2>
+        <table class="editorDemoTable">
+        <thead>
+        <tr>
+        <td>Name of the feature</td>
+        <td>Example</td>
+        <td>Default</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+        <td>Remove tag attributes</td>
+        <td><img style="margin: 1px 15px;" src="images/smiley.png" alt="laughing" width="40" height="16" /> (except <strong>img</strong>-<em>src</em> and <strong>a</strong>-<em>href</em>)</td>
+        <td>&nbsp;</td>
+        </tr>
+        <tr>
+        <td>Remove inline styles</td>
+        <td><span style="color: green; font-size: 13px;">You <strong style="color: blue; text-decoration: underline;">should never</strong>&nbsp;use inline styles!</span></td>
+        <td><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
+        </tr>
+        <tr>
+        <td>Remove classes and IDs</td>
+        <td><span id="demoId">Use classes to <strong class="demoClass">style everything</strong>.</span></td>
+        <td><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
+        </tr>
+        <tr>
+        <td>Remove all tags</td>
+        <td>This leaves <strong style="color: blue;">only the plain</strong> <em>text</em>. <img style="margin: 1px;" src="images/smiley.png" alt="laughing" width="16" height="16" /></td>
+        <td>&nbsp;</td>
+        </tr>
+        <tr>
+        <td>Remove successive &amp;nbsp;s</td>
+        <td>Never use non-breaking spaces&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;to set margins.</td>
+        <td><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
+        </tr>
+        <tr>
+        <td>Remove empty tags</td>
+        <td>Empty tags should go!</td>
+        <td>&nbsp;</td>
+        </tr>
+        <tr>
+        <td>Remove tags with one &amp;nbsp;</td>
+        <td>This makes&nbsp;no sense!</td>
+        <td><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
+        </tr>
+        <tr>
+        <td>Remove span tags</td>
+        <td>Span tags with <span style="color: green; font-size: 13px;">all styles</span></td>
+        <td><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
+        </tr>
+        <tr>
+        <td>Remove images</td>
+        <td>I am an image: <img src="images/smiley.png" alt="laughing" /></td>
+        <td>&nbsp;</td>
+        </tr>
+        <tr>
+        <td>Remove links</td>
+        <td><a href="https://html-online.com">This is</a> a link.</td>
+        <td>&nbsp;</td>
+        </tr>
+        <tr>
+        <td>Remove tables</td>
+        <td>Takes everything out of the table.</td>
+        <td>&nbsp;</td>
+        </tr>
+        <tr>
+        <td>Replace table tags with structured divs</td>
+        <td>This text is inside a table.</td>
+        <td>&nbsp;</td>
+        </tr>
+        <tr>
+        <td>Remove comments</td>
+        <td>This is only visible in the source editor <!-- HELLO! --></td>
+        <td><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
+        </tr>
+        <tr>
+        <td>Encode special characters</td>
+        <td><span style="color: red; font-size: 17px;">&hearts;</span> <strong style="font-size: 20px;">☺ ★</strong> &gt;&lt;</td>
+        <td><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
+        </tr>
+        <tr>
+        <td>Set new lines and text indents</td>
+        <td>Organize the tags in a nice tree view.</td>
+        <td>&nbsp;</td>
+        </tr>
+        </tbody>
+        </table>
+        <p><strong>&nbsp;</strong></p>
+        <p><strong>Save this link into your bookmarks and share it with your friends. It is all FREE! </strong><br /><strong>Enjoy!</strong></p>
+        <p><strong>&nbsp;</strong></p>`, // html body
       });
 
       console.log("Message sent: %s", info.messageId);
@@ -47,11 +156,12 @@ async function main() {
 
       // Preview only available when sending through an Ethereal account
       console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
- //   }
-  // catch(err)
-  //   {
-  //      console.error(`${new Date()} \n ERROR DETIALS : ${err}`);
-  //   }
+   }
+  catch(err)
+    {
+      throw err;
+       console.error(`${new Date()} \n ERROR DETIALS : ${err}`);
+    }
 }
 
 main().catch(console.error);
